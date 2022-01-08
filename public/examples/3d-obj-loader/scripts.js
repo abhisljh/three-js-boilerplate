51ef02a61866e5b2e3d8a6d46be2d7b9fb59973d
abhisljh <abhi.sljh@gmail.com>
2022-01-08 20:23:27 +0530

    commit

--- public/examples/3d-obj-loader/scripts.js
+++ public/examples/3d-obj-loader/scripts.js
@@ -1,7 +1,8 @@
 var scene = new THREE.Scene();
 
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-camera.position.z = 200;
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.5, 1000 );
+camera.position.z = 10;
+
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -35,19 +36,90 @@ mtlLoader.load('r2-d2.mtl', function (materials) {
     var objLoader = new THREE.OBJLoader();
     objLoader.setMaterials(materials);
     objLoader.setPath('/examples/3d-obj-loader/assets/');
-    objLoader.load('r2-d2.obj', function (object) {
-
+    objLoader.load('Buddha.obj', function (object) {
+		
+		
+		
         scene.add(object);
-        object.position.y -= 60;
-
-    });
-
-});
-
-var animate = function () {
+        object.position.y -= 2;
+		
+		
+		
+		var animate = function () {
 	requestAnimationFrame( animate );
 	controls.update();
 	renderer.render(scene, camera);
+	//object.rotation.x += 0.001;
+		object.rotation.y += 0.01;
+	
 };
 
 animate();
+
+
+
+
+
+
+		
+		
+
+
+	
+    });
+	
+	
+	//adding stars here 
+		
+		let starGeo, stars;
+		
+		starGeo = new THREE.Geometry();
+for(let i=0;i<6000;i++) {
+     star = new THREE.Vector3(
+    Math.random() * 600 - 300,
+    Math.random() * 600 - 300,
+    Math.random() * 600 - 300
+  );
+  
+  
+  star.velocity = 0;
+star.acceleration = 0.02;
+  starGeo.vertices.push(star);
+}
+
+
+let sprite = new THREE.TextureLoader().load( '/examples/3d-obj-loader/assets/starn.png' );
+let starMaterial = new THREE.PointsMaterial({
+  color: 0xaaaaaa,
+  size: 0.7,
+  map: sprite
+});
+
+
+stars = new THREE.Points(starGeo,starMaterial);
+scene.add(stars);
+		
+	animate();	
+		
+		    function animate() {
+      starGeo.vertices.forEach(p => {
+        p.velocity += p.acceleration
+        p.y -= p.velocity;
+        
+        if (p.y < -200) {
+          p.y = 200;
+          p.velocity = 0;
+        }
+      });
+      starGeo.verticesNeedUpdate = true; 
+	  stars.rotation.x +=0.003;
+	  renderer.render(scene,camera);
+	  requestAnimationFrame(animate);
+			}
+		
+		
+		
+		//ending stars here 
+
+});
+
